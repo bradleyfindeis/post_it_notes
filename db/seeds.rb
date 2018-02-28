e3e17f0e8494e3b731d74fc172354fcc5df7b76f
Bradley Findeis <bradleyfindeis@gmail.com>
2018-02-27 17:20:31 -0700

    created features

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+(10).times do
+  Note.create(
+  title: Faker::Hipster.sentence(4),
+  body: Faker::Hobbit.quote
+  )
+end
+puts "seeded"
